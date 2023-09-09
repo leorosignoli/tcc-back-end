085ed1a6969770bb2f4469f4eb4a768c24c4c109
leorosignoli <leonard.pereira2@ab-inbev.com>
2023-09-09 11:49:37 -0300

    Added base project

---
+++ calendar-event-manager/src/main/java/edu/uniceub/tcc/calendareventmanager/CalendarEventManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.uniceub.tcc.calendareventmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarEventManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarEventManagerApplication.class, args);
+	}
+
+}
---
+++ calendar-event-manager/src/test/java/edu/uniceub/tcc/calendareventmanager/CalendarEventManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uniceub.tcc.calendareventmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarEventManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
