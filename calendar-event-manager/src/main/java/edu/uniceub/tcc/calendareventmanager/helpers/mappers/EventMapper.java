338700205ea58d0b3fa190be4c2f629c0d2abff2
leorosignoli <leonard.pereira2@ab-inbev.com>
2023-09-12 02:28:07 -0300

    Fixed build stage

--- calendar-event-manager/src/main/java/edu/uniceub/tcc/calendareventmanager/helpers/mappers/EventMapper.java
+++ calendar-event-manager/src/main/java/edu/uniceub/tcc/calendareventmanager/helpers/mappers/EventMapper.java
@@ -1,19 +1,29 @@
 package edu.uniceub.tcc.calendareventmanager.helpers.mappers;
 
-import edu.uniceub.tcc.calendareventmanager.api.dtos.EventCreateAllRequest;
 import edu.uniceub.tcc.calendareventmanager.api.dtos.EventRequest;
 import edu.uniceub.tcc.calendareventmanager.models.Event;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface EventMapper {
 
-    Event toModel(EventCreateAllRequest eventCreateAllRequest);
+    Event toModel(EventRequest eventCreateAllRequest);
 
     @Mapping(target = "owner", source = "eventOwner")
-    List<Event> toModelList(List<EventRequest> eventCreateAllRequest, String eventOwner);
+    default List<Event> toModelList(List<EventRequest> eventCreateAllRequest, String eventOwner){
+
+        return eventCreateAllRequest.stream()
+                .map(this::toModel)
+                .map(event -> {
+                    event.setOwner(eventOwner);
+                    return event;
+                } )
+                .toList();
+    }
 
 }
