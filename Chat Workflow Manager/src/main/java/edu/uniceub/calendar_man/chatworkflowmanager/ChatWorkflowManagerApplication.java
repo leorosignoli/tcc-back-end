8a1949dd09387519d9792ec0a478effc7a96830f
leorosignoli <leonardo.pereira2@ab-inbev.com>
2023-09-29 15:04:31 -0300

    Base project

---
+++ Chat Workflow Manager/src/main/java/edu/uniceub/calendar_man/chatworkflowmanager/ChatWorkflowManagerApplication.java
@@ -0,0 +1,13 @@
+package edu.uniceub.calendar_man.chatworkflowmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatWorkflowManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatWorkflowManagerApplication.class, args);
+    }
+
+}
